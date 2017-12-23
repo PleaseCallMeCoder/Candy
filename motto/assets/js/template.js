bdabd5a84ec7543bee2627f93c75c2c8d2884e0d
renlei <renlei@yoloyolo.tv>
2017-12-23 16:46:17 +0800

    mod

--- motto/assets/js/template.js
+++ motto/assets/js/template.js
@@ -12,38 +12,38 @@ var template = (function($) { var _ = {
 						{  
 						    "thumbnail":"images/thumbs/01.jpg",  
 						    "image":"images/fulls/01.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"偏执",
+						    "description":"突然有些明白了为什么好多小说里最牛逼的两个人，一定要在结尾干一架？因为他们真的都认为自己是正确的，并且对此深信不疑。"
 						}, 
 						{  
 						    "thumbnail":"images/thumbs/02.jpg",  
 						    "image":"images/fulls/02.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"坚韧",
+						    "description":"遇到问题不要想着去逃避，也不要着急抱怨。姑且静下心来仔细想想，其实问题并没有想象中的复杂。所以，请别让抵触情绪阻止你前进的脚步。"
 						}, 
 						{  
 						    "thumbnail":"images/thumbs/03.jpg",  
 						    "image":"images/fulls/03.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"孤独",
+						    "description":"lonely kid hides in heart."
 						}, 
 						{  
 						    "thumbnail":"images/thumbs/04.jpg",  
 						    "image":"images/fulls/04.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"信仰",
+						    "description":"信自己也是一种信仰。"
 						}, 
 						{  
 						    "thumbnail":"images/thumbs/05.jpg",  
 						    "image":"images/fulls/05.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"学习",
+						    "description":"如果你想学好一件事情，那就去和这个领域里最牛逼的人去学。"
 						}, 
 						{  
 						    "thumbnail":"images/thumbs/06.jpg",  
 						    "image":"images/fulls/06.jpg",
-						    "title":"嘻嘻哈哈",
-						    "description":"嘻嘻哈哈哈哈哈哈哈"
+						    "title":"努力",
+						    "description":"如果你真的愿意去努力，你人生最坏的结果，也不过是大器晚成。"
 						}, 
 					   ];
 		var markup = '<article><a class="thumbnail" href="${image}" data-position="left center"><img src="${thumbnail}" alt="" /></a><h2>${title}</h2><p>${description}</p></article>';
